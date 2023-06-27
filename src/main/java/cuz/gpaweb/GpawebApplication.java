7a9fce58d2a801d6017f67830e23cfa8bc574ce6
DEVGRuSix <maoy7594@gmail.com>
2023-06-27 10:30:29 +0800

    Initial commit

---
+++ src/main/java/cuz/gpaweb/GpawebApplication.java
@@ -0,0 +1,19 @@
+package cuz.gpaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GpawebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GpawebApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "GPAweb";
+	}
+}
